36981690eb94d7ccab02fd36346b1e4b6bdb4267
Prajwal KR <prajwalkr1227@gmail.com>
2025-10-22 22:26:19 +0530

    Fix: Corrected case sensitivity for User model import in quizRoutes.js

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Quiz from "../models/Quiz.js";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 
